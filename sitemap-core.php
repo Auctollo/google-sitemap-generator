0d5ec641cecabed34d8f940b4dc71eaff187923f
SD <saurabh.d@toptal.com>
2021-09-03 16:46:38 +0530

    code refactor

--- sitemap-core.php
+++ sitemap-core.php
@@ -2274,17 +2274,17 @@ final class GoogleSitemapGenerator {
 	}
 
 	public function getXslUrl($url,$host){
-		if (substr($host, 0, 4) === 'www.') {
+		if (substr($host, 0, 4) === 'www.'){
 			if(substr(get_bloginfo('url'), 0, 5) != "https"){
 				if(strpos($url,"www.") === false){
 					$url = str_replace("http://","http://www.",$url);
 				}
 			} else{
-				if(strpos($url,"www.") == false){
+				if(strpos($url,"www.") === false){
 					$url = str_replace("https://","https://www.",$url);
 				}
 			}
-		}else{
+		} else{
 			if(strpos($url,"www.") !== false){
 				$url = str_replace("://www.","://",$url);
 			}
