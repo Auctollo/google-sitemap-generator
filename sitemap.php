ea68c8c4fe7cc07270e30edc1426a587cff087f4
SD <saurabh.d@toptal.com>
2022-04-21 18:59:28 +0530

    changes for latest version release

--- sitemap.php
+++ sitemap.php
@@ -17,7 +17,7 @@
  * Plugin Name: Google XML Sitemaps
  * Plugin URI: http://www.arnebrachhold.de/redir/sitemap-home/
  * Description: This plugin improves SEO using sitemaps for best indexation by search engines like Google, Bing, Yahoo and others.
- * Version: 4.1.2
+ * Version: 4.1.3
  * Author: Arne Brachhold
  * Author URI: http://www.arnebrachhold.de/
  * Text Domain: sitemap
