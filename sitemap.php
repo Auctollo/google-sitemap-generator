009d0c3fc49e1ec09c72aa452fa1f17072aa55cc
saurabhd <saurabhdhariwal.com@gmail.com>
2023-03-15 19:50:02 +0530

    changed gtm code

--- sitemap.php
+++ sitemap.php
@@ -78,15 +78,12 @@ function ga_header() {
 		$consent_value = get_option( 'sm_user_consent', $default_value );
 
 		if ( 'yes' === $consent_value || $default_value === $consent_value ) {
-			echo "
-			<script>
-				(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,'script','dataLayer','GTM-WT5FH38');
-				
-				</script>
+			echo "			
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-N8CQCXB');</script>
 				";
 		}
 		if ( $default_value !== $consent_value ) {
