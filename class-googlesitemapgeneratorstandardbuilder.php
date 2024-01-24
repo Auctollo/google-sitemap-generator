4789bf0d55bc21cd4b1295762d0f6af42710dbe9
Yuki Matsukura <matsubo@users.noreply.github.com>
2024-01-24 16:43:49 +0900

    Update class-googlesitemapgeneratorstandardbuilder.php
    fix for #164

--- class-googlesitemapgeneratorstandardbuilder.php
+++ class-googlesitemapgeneratorstandardbuilder.php
@@ -892,12 +892,14 @@ class GoogleSitemapGeneratorStandardBuilder {
 		}
 
 		$pages = $gsg->get_pages();
-		if ( count( $pages ) > 0 ) {
-			foreach ( $pages as $page ) {
-				$url = ! empty( $page->get_url() ) ? $page->get_url() : ( property_exists( $page, '_url' ) ? $page->_url : '' );
-				if ( $page instanceof GoogleSitemapGeneratorPage && $url ) {
-					$gsg->add_sitemap( 'externals-sitemap', null, $blog_update );
-					break;
+		if ($pages) {
+			if ( count( $pages ) > 0 ) {
+				foreach ( $pages as $page ) {
+					$url = ! empty( $page->get_url() ) ? $page->get_url() : ( property_exists( $page, '_url' ) ? $page->_url : '' );
+					if ( $page instanceof GoogleSitemapGeneratorPage && $url ) {
+						$gsg->add_sitemap( 'externals-sitemap', null, $blog_update );
+						break;
+					}
 				}
 			}
 		}
