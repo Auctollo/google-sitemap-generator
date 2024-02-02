c591d3fa0000ec237cc89b72d0b0cda2b9d67a10
Vadym Gmyria <admin@vcredit.kiev.ua>
2024-02-02 11:35:38 +0200

    fixed added kb data to memory calculation

--- sitemap-core.php
+++ sitemap-core.php
@@ -2178,7 +2178,11 @@ final class GoogleSitemapGenerator {
 		}
 		$end_time = microtime( true );
 		$end_time = round( $end_time - $start_time, 2 );
+		$spent_memory = intval((memory_get_peak_usage( true ) - $start_memory) / 1024);
+		if($spent_memory > 1023) $spent_memory = intval($spent_memory / 1024) . 'MB';
+		else $spent_memory .= 'KB';
 		$this->add_element( new GoogleSitemapGeneratorDebugEntry( 'Request ID: ' . md5( microtime() ) . '; Queries for sitemap: ' . ( $GLOBALS['wpdb']->num_queries - $start_queries ) . '; Total queries: ' . $GLOBALS['wpdb']->num_queries . "; Seconds: $end_time; Memory for sitemap: " . ( ( memory_get_peak_usage( true ) - $start_memory ) / 1024 / 1024 ) . 'MB; Total memory: ' . ( memory_get_peak_usage( true ) / 1024 / 1024 ) . 'MB' ) );
+		//$this->add_element( new GoogleSitemapGeneratorDebugEntry( 'Request ID: ' . md5( microtime() ) . '; Queries for sitemap: ' . ( $GLOBALS['wpdb']->num_queries - $start_queries ) . '; Total queries: ' . $GLOBALS['wpdb']->num_queries . "; Seconds: $end_time; Memory for sitemap: " . $spent_memory. '; Total memory: ' . ( memory_get_peak_usage( true ) / 1024 / 1024 ) . 'MB' ) );
 	}
 
 	/**
